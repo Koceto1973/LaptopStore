95e46a99539c07a420f7d33ede333a8685ed77cf
TeamK <koceto1973@yahoo.ca>
2018-12-27 20:57:01 +0200

    parsing the request

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');  // file system module in
 const http = require('http'); // server module in
+const url = require('url');  // routing module in, responding differntly on each url change
 
 // reading file from disk absolute path syncronously, __dirname is home folder
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
@@ -12,30 +13,35 @@ const laptopData = JSON.parse(json);  // parse the input into data object
 // calback having access to request and response objects
 const server = http.createServer((req, res) => {
     
-    console.log('Server got accessed!');
-
-
-    // const pathName = url.parse(req.url, true).pathname;
-    // const id = url.parse(req.url, true).query.id;
+    // console.log('Server got accessed!');
+    // console.log(req);  // previewing the request received from server
+    // console.log(req.url); // url part used for routing
     
-    // // PRODUCTS OVERVIEW
-    // if (pathName === '/products' || pathName === '/') {
-    //     res.writeHead(200, { 'Content-type': 'text/html'});
+    // putting url module at work
+    // console.log(url.parse(req.url, true));
+    const pathName = url.parse(req.url, true).pathname;  // true for parsing into object
+    // console.log(url.parse(req.url, true).query);
+    const id = url.parse(req.url, true).query.id;        // parsing the query object
+    
+    
+    // PRODUCTS OVERVIEW
+    if (pathName === '/products' || pathName === '/') {
+        // build response header
+        res.writeHead(200, { 'Content-type': 'text/html'});
         
-    //     fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
-    //         let overviewOutput = data;
+        // fs.readFile(`${__dirname}/templates/template-overview.html`, 'utf-8', (err, data) => {
+        //     let overviewOutput = data;
             
-    //         fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
+        //     fs.readFile(`${__dirname}/templates/template-card.html`, 'utf-8', (err, data) => {
             
-    //             const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
-    //             overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput); 
+        //         const cardsOutput = laptopData.map(el => replaceTemplate(data, el)).join('');
+        //         overviewOutput = overviewOutput.replace('{%CARDS%}', cardsOutput); 
                 
-    //             res.end(overviewOutput);
-    //         });
-    //     });
-        
-        
-    // }
+        //         // build response end
+        //         res.end(overviewOutput);
+        //     });
+        // });       
+    }
 });
 
 // node attaches listener on defined port, ip address ( usually local host for development ),
